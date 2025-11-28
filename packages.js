4dbf518aa66c5dadf65ed5db4c2b9d125419dbf9
eletino <157961694+eletino@users.noreply.github.com>
2025-11-28 22:11:16 +0100

    Create packages.js

---
+++ packages.js
@@ -0,0 +1,13 @@
+{
+  "name": "render-proxy",
+  "version": "1.0.0",
+  "type": "module",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "latest",
+    "http-proxy-middleware": "latest"
+  }
+}
